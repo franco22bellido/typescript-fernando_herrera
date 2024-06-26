fbbb1bf41b9f50418b3751a30a3fb64a297c6f56
franco22bellido <franco.bellido22@gmail.com>
2024-06-26 00:03:10 -0300

    async await, metodos asincronos

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 export class Pokemon {
     get imageUrl(): string {
         return `https://pokemon.com/${this.id}.jpg`
@@ -13,6 +15,10 @@ export class Pokemon {
     public speak() {
         console.log(` ${this.name}, ${this.name}`)
     }
+    async getMoves(){
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/4`)
+        return data.moves
+    }
 }
 
 export const charmander = new Pokemon(1, 'Charmander')
@@ -20,3 +26,5 @@ export const charmander = new Pokemon(1, 'Charmander')
 console.log(charmander.imageUrl)
 charmander.scream()
 charmander.speak()
+console.log(await charmander.getMoves())
+
