06b13f48760effa94a6f796fdb90012f77b027fa
franco22bellido <franco.bellido22@gmail.com>
2024-06-26 00:16:31 -0300

    generics

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { Move, PokeAPIResponse } from "../interface/pokeapi-response.interface"
 
 export class Pokemon {
     get imageUrl(): string {
@@ -15,8 +16,8 @@ export class Pokemon {
     public speak() {
         console.log(` ${this.name}, ${this.name}`)
     }
-    async getMoves(){
-        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/4`)
+    async getMoves(): Promise<Move[]>{
+        const { data } = await axios.get<PokeAPIResponse>(`https://pokeapi.co/api/v2/pokemon/4`)
         return data.moves
     }
 }
